5a82d162e08bf4fc751be825c392b3f8bece3a7b
André Oliveira <andreaoliveira@ua.pt>
2024-11-02 17:51:42 +0000

    add health check

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from db.create_database import create_tables
 from db.database import SessionLocal
 from routers import user, task
+from starlette import status
 
 
 @asynccontextmanager
@@ -38,6 +39,17 @@ app.add_middleware(
 
 app.include_router(user.router)
 app.include_router(task.router)
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+)
+def get_health():
+    return {"status": "ok"}
 
 
 @app.middleware("http")
